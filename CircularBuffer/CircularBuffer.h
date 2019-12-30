d939bd241a186ff9e1a1f9c763633bb09f71c59f
andrewyung <andrewyung@uvic.ca>
2019-12-30 05:04:22 -0500

    Implemented insert

--- CircularBuffer/CircularBuffer.h
+++ CircularBuffer/CircularBuffer.h
@@ -31,6 +31,25 @@ public:
 	}
 	void insert(BufferType toAdd, int atIndex) 
 	{
+		if (atIndex >= _size)
+		{
+			std::stringstream ss;
+			ss << "Insert at " << atIndex << " is out of bounds. Size: " << _size;
+			throw std::out_of_range(ss.str());
+		}
+
+		// Shift all elements right of atIndex right by 1
+		for (int i = _size; i > atIndex; i--)
+		{
+			_indexToMemIndexTable[i] = _indexToMemIndexTable[i - 1];
+		}
+
+		int availableIndex = _freeMemIndexStack[_currentFreeMemIndex];
+		_pointerArray[availableIndex] = toAdd;
+		_indexToMemIndexTable[atIndex] = availableIndex;
+
+		_currentFreeMemIndex++;
+		_size++;
 	}
 	void remove() {}
 	BufferType get(int atIndex) 
@@ -44,11 +63,12 @@ public:
 
 private:
 	std::unique_ptr<BufferType[]> _pointerArray;
-	std::unique_ptr<int[]> _indexToMemIndexTable, _freeMemIndexStack;
+	std::unique_ptr<int[]> _indexToMemIndexTable; // -1 means index not used 
+	std::unique_ptr<int[]> _freeMemIndexStack;
 
-	int _currentFreeMemIndex;
+	int _currentFreeMemIndex = 0;
 	unsigned int _capacity;
-	unsigned int _size;
+	unsigned int _size = 0;
 
 	void resize()
 	{
