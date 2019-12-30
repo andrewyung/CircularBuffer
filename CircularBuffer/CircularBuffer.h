9a5f5ff87f45155b11e2480256b5d354e37b6fbe
andrewyung <andrewyung@uvic.ca>
2019-12-30 01:48:19 -0500

    Implementation of add(BufferType toAdd)

--- CircularBuffer/CircularBuffer.h
+++ CircularBuffer/CircularBuffer.h
@@ -1,28 +1,55 @@
 #pragma once
 
+#include <memory>
+#include <iostream>
+
 template <typename BufferType>
 
 class CircularBuffer
 {
 public:
-	CircularBuffer();
-	CircularBuffer(unsigned int capacity);
-	~CircularBuffer();
+	CircularBuffer(unsigned int capacity = 10)
+	{
+		_capacity = capacity;
 
-	void add();
-	void get();
-	void remove();
-	unsigned int size();
+		std::unique_ptr<BufferType[]> pointerArray(new BufferType[capacity]);
+
+		std::unique_ptr<int[]> indexToMemIndexTable(new int[capacity]); // -1 means not used
+		std::unique_ptr<int[]> freeMemIndexStack(new int[capacity]);  //  -1 means occupied slot
+	}
+
+	void add(BufferType toAdd) 
+	{
+		int availableIndex = _freeMemIndexStack[_currentFreeMemIndex];
+		_pointerArray[availableIndex] = toAdd;
+		_indexToMemIndexTable[_size] = availableIndex;
+		
+		_currentFreeMemIndex++;
+		_size++;
+	}
+	void insert(BufferType toAdd, int atIndex) 
+	{
+	}
+	void remove() {}
+	BufferType get(int atIndex) 
+	{
+		return _pointerArray[_indexToMemIndexTable[atIndex]];
+	}
+	unsigned int size() 
+	{
+		return _size;
+	}
 
 private:
-	struct DataNode
+	std::unique_ptr<BufferType[]> _pointerArray;
+	std::unique_ptr<int[]> _indexToMemIndexTable, _freeMemIndexStack;
+
+	int _currentFreeMemIndex;
+	unsigned int _capacity;
+	unsigned int _size;
+
+	void resize()
 	{
-		T data;
-		DataNode *next;
-	};
 
-	DataNode<BufferType>* head;
-	DataNode<BufferType>* tail;
-
-	void resize();
+	}
 };
